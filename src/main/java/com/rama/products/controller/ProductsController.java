586af980e94bfb5a04af30f66f8629bcf9bba4df
Sahana20212 <96504836+Sahana20212@users.noreply.github.com>
2022-12-21 18:10:01 -0500

    changes related to exception and queries

--- src/main/java/com/rama/products/controller/ProductsController.java
+++ src/main/java/com/rama/products/controller/ProductsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rama.products.entity.Products;
+import com.rama.products.error.ProductNotFoundException;
 import com.rama.products.service.ProductsService;
 
 @RestController
@@ -21,7 +22,7 @@ public class ProductsController {
 	
 	
 	@PostMapping("/products")
-	public Products saveProducts(@RequestBody @Valid Products products) {
+	public Products saveProducts(@RequestBody @Valid Products products) throws ProductNotFoundException {
 		return productsservice.saveProducts(products);
 	}
 
--- src/main/java/com/rama/products/service/ProductsService.java
+++ src/main/java/com/rama/products/service/ProductsService.java
@@ -1,12 +1,14 @@
 package com.rama.products.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.rama.products.entity.Products;
+import com.rama.products.error.ProductNotFoundException;
 
 public interface ProductsService {
 
-	  Products saveProducts(Products products);
+	  Products saveProducts(Products products) throws ProductNotFoundException;
 	  public List<Products> fetchProductstList();
 	List<Products> fetchProductsList();
 	
